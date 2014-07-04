6d281de248e9052fde15adc921c8c1b8350e156e
Abhinav Agarwalla <navabhi174.gmail.com>
2014-07-04 15:49:56 +0530

    trying out live video

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -18,4 +18,24 @@ class RegistrationsController < Devise::RegistrationsController
   def update
     super
   end
+  
+=begin
+  def after_sign_up_path_for(resource)
+    user = User.where(email: params[:user][:email]).first
+    std_record = user.students.first
+    tcr_record = user.teachers.first
+
+    
+    puts "                 \n\n  hi  \n\n\n"
+    puts std_record
+    puts tcr_record
+
+    if std_record != nil
+      "/students/#{std_record.id}"
+    else
+      "/teachers/#{tcr_record.id}"
+    end
+
+  end
+=end
 end 
